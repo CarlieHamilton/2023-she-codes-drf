8949a70d23e77e56b4bb3621748c42e20cdffbab
CarlieHamilton <carliemhamilton@gmail.com>
2023-08-08 18:22:38 +1000

    typo

--- crowdfunding/users/serializers.py
+++ crowdfunding/users/serializers.py
@@ -5,7 +5,7 @@ class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = '__all__'
-        extra_kwargs = {'password': {'write_only: True'}}
+        extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         return CustomUser.objects.create(**validated_data)
